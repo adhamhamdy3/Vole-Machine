0e87e7a227d77e51cc2e5c450acff93e6f451c71
Youssef-Bahaa <162743130+Youssef-Bahaa@users.noreply.github.com>
2024-10-26 01:56:42 +0300

    ALU.h

--- ALU.h
+++ ALU.h
@@ -13,6 +13,13 @@ public:
     static double hexToDecFloat(const string& hexValue);
     static string decToHex(int decimalValue);
     static string decToHexFloat(double decimalValue);
+    static short hexToDec(const string&);
+    static short hexToDec(const string&);
+    static bool isHex(const string&);
+    static bool isValid(const string&);
+    static bool isValidRegIdx(const short&);
+    static bool isValidMemIdx(const short&);
+    static bool containsLowerCaseHex(const string& hexStr);
 };
 
 
