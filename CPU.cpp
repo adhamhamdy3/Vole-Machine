c63d47004d4adcdf9c68ff2253e6ddd85ae481a1
Youssef-Bahaa <162743130+Youssef-Bahaa@users.noreply.github.com>
2024-11-04 23:00:56 +0200

    Update CPU.cpp

--- CPU.cpp
+++ CPU.cpp
@@ -2,7 +2,7 @@
 #include "Machine.h"
 
 
-CPU::CPU(ALU *aluPtr, CU *cuPtr, Register *regPtr) : pc{0}, ir{"0000"}, registers{regPtr}, alu{aluPtr}, cu{cuPtr} {
+CPU::CPU(ALU* aluPtr, CU* cuPtr, Register* regPtr) : pc{ 0 }, ir{ "0000" }, registers{ regPtr }, alu{ aluPtr }, cu{ cuPtr } {
     decodeMessage = "NOTHING. Advance to the next step.";
 }
 
@@ -12,15 +12,15 @@ CPU::~CPU() {
     delete registers;
 }
 
-void CPU::fetchInstruction(Memory *mem) {
+void CPU::fetchInstruction(Memory* mem) {
     string byte1, byte2;
 
     byte1 = mem->getCell(pc++);
-    if(pc + 1 >= 256) return void (pc = 0);
+    if (pc + 1 >= 256) return void(pc = 0);
 
 
     byte2 = mem->getCell(pc++);
-    if(pc + 1 >= 256) return void (pc = 0);
+    if (pc + 1 >= 256) return void(pc = 0);
 
     ir = byte1 + byte2;
 }
@@ -41,33 +41,33 @@ void CPU::clearRegister() const {
 bool CPU::executeInstruction(Memory* mem) {
     char op_code = ir[0];
     switch (op_code) {
-        case '1':
-            cu->load1(this, mem);
-            break;
-        case '2':
-            cu->load2(this);
-            break;
-        case '3':
-            if (ir.substr(2) != "00")
-                cu->store1(this, mem);
-            else
-                cu->store2(this, mem);
-            break;
-        case '4':
-            cu->move(this);
-            break;
-        case '5':
-            cu->add1(this);
-            break;
-        case '6':
-            cu->add2(this);
-            break;
-        case 'B':
-            cu->jump(this);
-            break;
-        case 'C':
-          //  cout << "Exit..." << endl;
-            Machine::running = false;
+    case '1':
+        cu->load1(this, mem);
+        break;
+    case '2':
+        cu->load2(this);
+        break;
+    case '3':
+        if (ir.substr(2) != "00")
+            cu->store1(this, mem);
+        else
+            cu->store2(this, mem);
+        break;
+    case '4':
+        cu->move(this);
+        break;
+    case '5':
+        cu->add1(this);
+        break;
+    case '6':
+        cu->add2(this);
+        break;
+    case 'B':
+        cu->jump(this);
+        break;
+    case 'C':
+        //cout << "Exit..." << endl;
+        Machine::running = false;
 
     }
     return Machine::running;
@@ -81,34 +81,34 @@ void CPU::decodeInstruction() {
         break;
     case '1':
         decodeMessage = "LOAD Register " + string(1, ir[1]) +
-                        " with the content of memory at address " + ir.substr(2) + ".";
+            " with the content of memory at address " + ir.substr(2) + ".";
         break;
     case '2':
         decodeMessage = "LOAD Register " + string(1, ir[1]) +
-                        " with bit pattern " + ir.substr(2) + ".";
+            " with bit pattern " + ir.substr(2) + ".";
         break;
     case '3':
         decodeMessage = "STORE the bit pattern of Register " + string(1, ir[1]) +
-                        " in memory at address " + ir.substr(2) + ".";
+            " in memory at address " + ir.substr(2) + ".";
         break;
     case '4':
         decodeMessage = "MOVE the bit pattern of Register " + string(1, ir[2]) +
-                        " into Register " + string(1, ir[3]) + ".";
+            " into Register " + string(1, ir[3]) + ".";
         break;
     case '5':
         decodeMessage = "ADD the bit patterns of Registers " + string(1, ir[2]) +
-                        " and " + string(1, ir[3]) + " as if they were integers, and store the result in Register " +
-                        string(1, ir[1]) + ".";
+            " and " + string(1, ir[3]) + " as if they were integers, and store the result in Register " +
+            string(1, ir[1]) + ".";
         break;
     case '6':
         decodeMessage = "ADD the bit patterns of Registers " + string(1, ir[2]) +
-                        " and " + string(1, ir[3]) + " if they were floating point numbers, and store the result in Register " +
-                        string(1, ir[1]) + ".";
+            " and " + string(1, ir[3]) + " if they were floating point numbers, and store the result in Register " +
+            string(1, ir[1]) + ".";
         break;
     case 'B':
         decodeMessage = "JUMP to the instruction located in memory at address " + ir.substr(2) +
-                        " if the bit pattern in Register " + string(1, ir[1]) +
-                        " matches with the bit pattern in Register 0.";
+            " if the bit pattern in Register " + string(1, ir[1]) +
+            " matches with the bit pattern in Register 0.";
         break;
     case 'C':
         decodeMessage = "HALT the execution.";
@@ -117,9 +117,12 @@ void CPU::decodeInstruction() {
         decodeMessage = "Unknown operation code.";
         break;
     }
+    cout << "PC: " << pc << endl;
+    cout << "IR: " << ir << endl;
+    cout << decodeMessage << endl;
 }
 
-void CPU::runOneCycle(Memory* mem){
+void CPU::runOneCycle(Memory* mem) {
     CPU::fetchInstruction(mem);
     CPU::decodeInstruction();
     CPU::executeInstruction(mem);
