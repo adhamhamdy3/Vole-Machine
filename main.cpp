23c51ab803d37a60b8b5800c1b98d9bd6d71edc8
Adham Hamdy <160431507+adhamhamdy3@users.noreply.github.com>
2024-11-06 20:19:00 +0200

    run until halt and coloring issue fixed + icon

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include "mainwindow.h"
-
+#include<QIcon>
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    a.setWindowIcon(QIcon(":/images/volemachine.ico"));
+    a.setWindowIcon(QIcon(":/images/vole-machine.ico"));
     MainWindow w;
     w.show();
     return a.exec();
